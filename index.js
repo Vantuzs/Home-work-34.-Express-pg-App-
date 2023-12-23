0cfbed8ef16662926722514af4535c2bbe724607
Viktoriia <pecourses>
2023-12-23 18:29:16 +0200

    Add cb vs promise syntax

--- index.js
+++ index.js
@@ -13,7 +13,36 @@ const pool = new Pool(connectionOptions);
 // Завершити з'єднання з БД при завершенні роботи застосунку
 process.on('beforeExit', () => pool.end());
 
+// promise then/catch
 pool
   .query('SELECT CURRENT_DATE')
-  .then(res => console.log('res :>> ', res.rows[0]))
+  .then(res => console.log('1res :>> ', res.rows[0]))
   .catch(err => console.log('err :>> ', err));
+
+// cb
+pool.query('SELECT CURRENT_DATE', (err, res) => {
+  if (!err) {
+    console.log('2res.rows[0] :>> ', res.rows[0]);
+  }
+});
+
+// promise async/await
+(async function () {
+  try {
+    const res = await pool.query('SELECT CURRENT_DATE');
+    console.log('3res.rows[0] :>> ', res.rows[0]);
+  } catch (err) {
+    console.log('err :>> ', err);
+  }
+})();
+
+// Отримати список кастомерів
+// pool
+//   .query(
+//     `
+//     SELECT *
+//     FROM customers
+//     `
+//   )
+//   .then(res => console.log('res', res.rows))
+//   .catch(err => console.log('err', err));
