16b54ccd3c4b79a314b59da98fd59fac9645cd09
Viktoriia <pecourses>
2023-12-23 19:30:53 +0200

    Add User class create

--- index.js
+++ index.js
@@ -83,40 +83,76 @@ process.on('beforeExit', () => pool.end());
 const fn = 'Petro1';
 const ln = 'Petrenko1';
 
-(async () => {
-  try {
-    const customer = await pool.query(
-      `
-      SELECT *
-      FROM customers
-      WHERE first_name = $1 AND last_name = $2 
-    `,
-      [fn, ln]
-    );
-    console.log('customer.rows[0] :>> ', customer.rows[0]);
-  } catch (err) {
-    console.log('err :>> ', err);
-  }
-})();
+// (async () => {
+//   try {
+//     const customer = await pool.query(
+//       `
+//       SELECT *
+//       FROM customers
+//       WHERE first_name = $1 AND last_name = $2
+//     `,
+//       [fn, ln]
+//     );
+//     console.log('customer.rows[0] :>> ', customer.rows[0]);
+//   } catch (err) {
+//     console.log('err :>> ', err);
+//   }
+// })();
 
 // Створити замовлення користувачу с id=1
 const customer_id = 1;
 const created_at = '2023-10-10';
 
-(async () => {
-  try {
-    const order = await pool.query(
-      `
-      INSERT INTO orders (customer_id, created_at)
-      VALUES ($1, $2)
-      RETURNING *
-    `,
-      [customer_id, created_at]
-    );
-    console.log('customer.rows[0] :>> ', order.rows[0]);
-  } catch (err) {
-    console.log('err :>> ', err);
-  }
-})();
+// (async () => {
+//   try {
+//     const order = await pool.query(
+//       `
+//       INSERT INTO orders (customer_id, created_at)
+//       VALUES ($1, $2)
+//       RETURNING *
+//     `,
+//       [customer_id, created_at]
+//     );
+//     console.log('customer.rows[0] :>> ', order.rows[0]);
+//   } catch (err) {
+//     console.log('err :>> ', err);
+//   }
+// })();
 
 //////////////////////////////////////////////////////////
+
+// POST /customers {}
+// GET /customers?page=1&results=5
+// GET /customers/1
+// PATCH /customers/1 {}
+// DELETE /customers/1
+
+class User {
+  static async create ({ firstName, lastName, email, tel }) {
+    try {
+      // сформувати запит
+      const insertQuery = `
+        INSERT INTO customers (first_name, last_name, email, tel)
+        VALUES ('${firstName}', '${lastName}', '${email}', '${tel}')
+        RETURNING *
+      `;
+      const createdCustomer = await pool.query(insertQuery); // виконати його
+      return createdCustomer.rows[0]; // повернути результат
+    } catch (err) {
+      console.log('err :>> ', err); // або помилку
+    }
+  }
+  static getAll ({ limit, offset }) {}
+  static getById (id) {}
+  static updateById (id, { firstName, lastName, email, tel }) {}
+  static deleteById (id) {}
+}
+
+const newCustomer = {
+  firstName: 'Test',
+  lastName: 'testovych',
+  email: 'mail100@gmail.com',
+  tel: '+380123456780',
+};
+
+User.create(newCustomer).then(data => console.log('data :>> ', data));
