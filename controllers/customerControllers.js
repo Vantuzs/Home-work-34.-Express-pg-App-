94ea6503db6a18fbd4a6870ec0528b6775d5d8cc
Viktoriia <pecourses>
2023-12-24 17:42:19 +0200

    Add GET single customer

--- controllers/customerControllers.js
+++ controllers/customerControllers.js
@@ -1,10 +1,28 @@
+const { Customer } = require('./../models');
+
 module.exports.createCustomer = (req, res) => {};
 
 module.exports.getAllCustomers = (req, res) => {
   res.send('ok');
 };
 
-module.exports.getByIdCustomer = (req, res) => {};
+module.exports.getByIdCustomer = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const foundCustomer = await Customer.getById(id);
+
+    if (!foundCustomer) {
+      // TODO createHttpError
+      return res.status(404).send('Customer Not Found');
+    }
+
+    res.status(200).send(foundCustomer);
+  } catch (err) {
+    // TODO next(err)
+    res.status(500).send('Server Error');
+  }
+};
 
 module.exports.updateByIdCustomer = (req, res) => {};
 
